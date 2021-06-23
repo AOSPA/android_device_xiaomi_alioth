cca9138841205cbb5e702b2dbf67a6faad851cec
UtsavBalar1231 <utsavbalar1231@gmail.com>
2021-06-23 18:56:36 +0530

    alioth: Declare fingerprint hardware support
    Change-Id: I55d98610e09e2d8b40a865a5106340d1b2ba95e9

--- device.mk
+++ device.mk
@@ -126,7 +126,8 @@ PRODUCT_PACKAGES += \
 # Fingerprint
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/fingerprint/uinput-fpc.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/uinput-fpc.kl \
-    $(LOCAL_PATH)/configs/fingerprint/uinput-fpc.idc:$(TARGET_COPY_OUT_VENDOR)/usr/idc/uinput-fpc.idc
+    $(LOCAL_PATH)/configs/fingerprint/uinput-fpc.idc:$(TARGET_COPY_OUT_VENDOR)/usr/idc/uinput-fpc.idc \
+    frameworks/native/data/etc/android.hardware.fingerprint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.fingerprint.xml
 
 # FRP
 PRODUCT_PROPERTY_OVERRIDES += \
