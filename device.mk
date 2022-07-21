45d3920baf91ebda73c79a1b40292db79278f49c
UtsavBalar1231 <utsavbalar1231@gmail.com>
2022-07-21 14:32:31 +0000

    alioth: camera: Import camxoverride settings configuration
    From V13.0.5.0.SKHMIXM
    
    Change-Id: Id5dfd67f1f0f5a216ac5f4ae613643b3f44d35e6
    Signed-off-by: UtsavBalar1231 <utsavbalar1231@gmail.com>
    Signed-off-by: Omkar Chandorkar <gotenksIN@aospa.co>

--- device.mk
+++ device.mk
@@ -118,6 +118,7 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.camera.raw.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.raw.xml
 
 PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/camera/camxoverridesettings.txt:$(TARGET_COPY_OUT_VENDOR)/etc/camera/camxoverridesettings.txt \
     $(LOCAL_PATH)/configs/camera/st_license.lic:$(TARGET_COPY_OUT_VENDOR)/etc/camera/st_license.lic
 
 PRODUCT_PACKAGES += \
