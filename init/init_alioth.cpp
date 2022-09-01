45b609c288f08376a73ba2bbf6c13b9db1d0dfc3
Albert I <kras@raphielgang.org>
2022-09-02 00:32:35 +0900

    alioth: libinit: override product props on all partitions
    Change-Id: Ia8e9722583212ea67793b4d99da84eec100d023a
    Signed-off-by: Omkar Chandorkar <gotenksIN@aospa.co>

--- init/init_alioth.cpp
+++ init/init_alioth.cpp
@@ -1,34 +1,25 @@
-/*
- * Copyright (C) 2021 Paranoid Android
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-#include <vector>
+//
+// Copyright (C) 2021-2022 KudProject Development
+// Copyright (C) 2021 Paranoid Android
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+
+#include <cstdio>
 
 #include <android-base/properties.h>
 #define _REALLY_INCLUDE_SYS__SYSTEM_PROPERTIES_H_
 #include <sys/_system_properties.h>
 #include <sys/sysinfo.h>
 
-using android::base::GetProperty;
+void property_override(char const prop[], char const value[])
+{
+    prop_info *pi;
 
-void property_override(char const prop[], char const value[], bool add = true) {
-    prop_info* pi;
-
-    pi = (prop_info*)__system_property_find(prop);
+    pi = (prop_info*) __system_property_find(prop);
     if (pi)
         __system_property_update(pi, value, strlen(value));
-    else if (add)
+    else
         __system_property_add(prop, strlen(prop), value, strlen(value));
 }
 
@@ -36,22 +27,73 @@ void load_dalvikvm_properties() {
     struct sysinfo sys;
     sysinfo(&sys);
     if(sys.totalram > 6144ull * 1024 * 1024) {
-    // from - phone-xhdpi-8192-dalvik-heap.mk
-        property_override("dalvik.vm.heapstartsize", "24m");
-        property_override("dalvik.vm.heaptargetutilization", "0.46");
-        property_override("dalvik.vm.heapmaxfree", "48m");
+        // from - phone-xhdpi-8192-dalvik-heap.mk
+        property_override("dalvik.vm.heapstartsize", "24m", true);
+        property_override("dalvik.vm.heaptargetutilization", "0.46", true);
+        property_override("dalvik.vm.heapmaxfree", "48m", true);
     }
 }
 
-void vendor_load_properties() {
-    std::string region = GetProperty("ro.boot.hwc", "");
-    if (region.find("INDIA") != std::string::npos) {
-        property_override("ro.product.model", "M2012K11AI");
-    } else if (region.find("CN") != std::string::npos) {
-        property_override("ro.product.model", "M2012K11AC");
-    } else {
-        property_override("ro.product.model", "M2012K11AG");
+#define MAX_PROP_LIST 8
+void property_override_multiple(std::string type, char const value[], bool is_product)
+{
+    const std::string build_prop_list[MAX_PROP_LIST] = {
+        "ro.bootimage.build.",
+        "ro.build.",
+        "ro.odm.build.",
+        "ro.product.build.",
+        "ro.system.build.",
+        "ro.system_ext.build.",
+        "ro.vendor.build.",
+        "ro.vendor_dlkm.build.",
+    };
+
+    const std::string product_prop_list[MAX_PROP_LIST] = {
+        "ro.product.",
+        "ro.product.bootimage.",
+        "ro.product.odm.",
+        "ro.product.product.",
+        "ro.product.system.",
+        "ro.product.system_ext.",
+        "ro.product.vendor.",
+        "ro.product.vendor_dlkm.",
+    };
+
+    for (int i = 0; i < MAX_PROP_LIST; i++) {
+        if (is_product)
+            property_override((char *)&(product_prop_list[i] + type)[0], value);
+        else
+            property_override((char *)&(build_prop_list[i] + type)[0], value);
     }
+}
+
+void vendor_load_properties()
+{
+    std::string region = android::base::GetProperty("ro.boot.hwc", "");
+    char const *brand, *device, *model, *name;
+
+    if (region == "CN") {
+        brand = "Redmi";
+        device = "alioth";
+        model = "M2012K11AC";
+        name = "alioth";
+    } else if (region == "GLOBAL") {
+        brand = "POCO";
+        device = "alioth";
+        model = "M2012K11AG";
+        name = "alioth_global";
+    } else if (region == "INDIA") {
+        brand = "Mi";
+        device = "aliothin";
+        model = "M2012K11AI";
+        name = "aliothin";
+    }
+
+    property_override_multiple("brand", brand, true);
+    property_override_multiple("device", device, true);
+    property_override_multiple("marketname", marketname, true);
+    property_override_multiple("model", model, true);
+    property_override_multiple("name", name, true);
 
     load_dalvikvm_properties();
 }
