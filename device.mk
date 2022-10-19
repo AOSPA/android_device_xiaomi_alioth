18b7bc83eb8d3a4f6201249399b41cdbaf38ac22
Omkar Chandorkar <gotenksIN@aospa.co>
2022-10-19 14:37:43 +0900

    alioth: add displayfeature and citsensorservice
    - thanks xiaomi for not using standard QDCM
    - *should* fix non-existent HBM in HDR profile and OLED Black Crush
    
    Change-Id: I80cb6a238c99971e9445fabd4d268a46e324d567
    Signed-off-by: Omkar Chandorkar <gotenksIN@aospa.co>

--- device.mk
+++ device.mk
@@ -164,8 +164,13 @@ PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
     ro.surface_flinger.set_display_power_timer_ms=1000 \
     ro.surface_flinger.use_content_detection_for_refresh_rate=true
 
-PRODUCT_COPY_FILES += \
-    $(call find-copy-subdir-files,*,$(LOCAL_PATH)/configs/display/,$(TARGET_COPY_OUT_VENDOR)/etc)
+PRODUCT_VENDOR_PROPERTIES += \
+    persist.vendor.dc_backlight.enable=false \
+    persist.vendor.dc_backlight.threshold=440 \
+    ro.vendor.histogram.enable=true \
+    ro.vendor.smart_dfps.enable=false \
+    ro.vendor.sre=1 \
+    ro.vendor.xiaomi.bl.poll=true
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/displayconfig/display_id_4630946736638489729.xml:$(TARGET_COPY_OUT_VENDOR)/etc/displayconfig/display_id_4630946736638489729.xml
