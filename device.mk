b7c8e1181f37ca993220a9f8624139abb9fc0e9a
Omkar Chandorkar <gotenksIN@aospa.co>
2023-05-02 21:38:19 +0900

    alioth: update audiopolicy to miui_ALIOTHGlobal_V14.0.4.0.TKHMIXM
    - also copy audiopolicy to vendor/etc/audio
    
    Change-Id: I1d79226b40c55864598ac3205f8c584506ac9223
    Signed-off-by: Omkar Chandorkar <gotenksIN@aospa.co>

--- device.mk
+++ device.mk
@@ -72,6 +72,7 @@ PRODUCT_ODM_PROPERTIES += \
     vendor.audio.offload.track.enable=false
 
 PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/audio/audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio/audio_policy_configuration.xml \
     $(call find-copy-subdir-files,*,$(LOCAL_PATH)/configs/audio/,$(TARGET_COPY_OUT_VENDOR)/etc)
 
 # Bluetooth
