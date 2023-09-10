11c4d21d46586eec9c39c08b456cf9254023dbb9
Adithya R <gh0strider.2k18.reborn@gmail.com>
2023-09-10 10:15:25 -0300

    alioth: parts: Introduce Dolby Atmos
    Based on existing dirac implementation and observing stock
    sound effects app and daxservice.
    
    Thanks to jhenrique09 for the hack from old dirac parts
    "Pause/play music stream to get effects applied".
    
    TODO: bring back misound (same as stock)
    
    Co-authored-by: Henrique Silva <jhenrique09.mcz@hotmail.com>
    Change-Id: Ia137963efee2f54a74b391dc5ea5016fc60a8fab
    Signed-off-by: Mesquita <mesquita@aospa.co>

--- device.mk
+++ device.mk
@@ -74,6 +74,9 @@ PRODUCT_ODM_PROPERTIES += \
 PRODUCT_COPY_FILES += \
     $(call find-copy-subdir-files,*,$(LOCAL_PATH)/configs/audio/,$(TARGET_COPY_OUT_VENDOR)/etc)
 
+PRODUCT_PACKAGES += \
+    DolbyManager
+
 # Bluetooth
 PRODUCT_VENDOR_PROPERTIES += \
     persist.sys.fflag.override.settings_bluetooth_hearing_aid=true \
