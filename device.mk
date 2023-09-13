49da2695c48deb254917ae326efd6afe094dd4d4
Adithya R <gh0strider.2k18.reborn@gmail.com>
2023-09-13 16:23:25 -0300

    alioth: Add support for high brightness mode (HBM)
    * Brightness from 2047 (488~ nits) to 4095 (800 nits) is HBM.
      Enter HBM at >=6500 lux (outdoor daylight) and hit peak brightness
      at >=10000 lux (under direct sunlight). Adjust the backlight curve
      accordingly.
    
    * [rk134]: To get the uniqueID for the display, run the following command:
               adb shell dumpsys display | grep uniqueId
    
    Co-authored-by: Mesquita <mesquita@aospa.co>
    Change-Id: Ia8a53e00adf09ffc731d6cc2fb8e62a368744dea

--- device.mk
+++ device.mk
@@ -181,6 +181,9 @@ PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
 PRODUCT_COPY_FILES += \
     $(call find-copy-subdir-files,*,$(LOCAL_PATH)/configs/display/,$(TARGET_COPY_OUT_VENDOR)/etc)
 
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/displayconfig/display_id_4630946736638489729.xml:$(TARGET_COPY_OUT_VENDOR)/etc/displayconfig/display_id_4630946736638489729.xml
+
 # DPM
 PRODUCT_VENDOR_PROPERTIES += \
     persist.vendor.dpmhalservice.enable=1
